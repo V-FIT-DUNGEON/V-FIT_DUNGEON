10c2f57fdd9aca596749859cb90ae10babf08cf8
Maroj Thangthong <maroj.t@ku.th>
2025-02-14 17:15:55 +0700

    Add all body part button to UI

--- Assets/scripts/UI_scripts/Creative_btn.cs
+++ Assets/scripts/UI_scripts/Creative_btn.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//This scrip make hit box of botton follow the picture exlude transparent part
 public class Creative_btn : MonoBehaviour
 {
+    [SerializeField] float alphaHitTestMinimumThreshold = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = alphaHitTestMinimumThreshold;
     }
 
     // Update is called once per frame
